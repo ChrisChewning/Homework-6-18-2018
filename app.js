22781957841825ab5ef96f03d1fa8f1bc60c95cc
Chris <chewning.cl@gmail.com>
2018-06-18 17:13:34 -0500

    1st commit - selecting datatypes.

---
+++ app.js
@@ -0,0 +1,68 @@
+console.log('hi');
+
+// So far we have seen:
+//
+// Strings
+// Numbers
+// Booleans
+// Arrays
+// Objects
+// When these datatypes are combined, we get a data structure, for example, an array that contains objects that each contain booleans.
+//
+// Let's say you are tasked with creating some software, and it is up to you to determine which datatypes and what data structure to use.
+//
+// Answer the Following
+// For each of the following, write which datatypes you would use to represent the data, and then give a small example of the data structure:
+
+// A light switch that can be either on or off.
+// //A boolean where true: on and false: off
+// if (lightswitch == 'on') {
+//   return true;
+// }
+//
+//
+// // A user's email address.
+// //a string b.c you need to be able to encompass numbers.
+// windows.alert('Email address here, please:');
+//
+// // A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// //an object named spaceship.
+// const spaceschip = {
+//   hull: '',
+//   laser blaster: true,
+//   tractor beam: '25 feet',
+//   warp drive: 10
+// }
+//
+// // A list of student names from our class.
+// //an array b.c that's all you need.
+// const namesOfPartyPotensh = [];
+//
+// // A list of student names from our class, each with a location.
+// an object with a key: value pair
+// // const names = {
+//  [
+//   name: location
+// ]
+
+// A list of student names from our class, each with a location and
+//each with a list of favorite tv shows.
+
+// // an object.
+// const profiles = {
+//   name: '',
+//   location: '',
+//   favorite tv shows: [
+//
+//   ]
+//   }
+// }
+
+
+#2:
+
+
+Make an array that holds all of the colors of the rainbow.
+Write code that will access "blue" from the rainbow array.
+Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+Write code that will access your hobby from the object that you just created.
