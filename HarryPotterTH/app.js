6413d43f9e23b9fffb707e5c4f7886b938ad8cad
Chris <chewning.cl@gmail.com>
2018-06-21 23:05:00 -0500

    Commit 3 - got it all to work right.

--- HarryPotterTH/app.js
+++ HarryPotterTH/app.js
@@ -5,32 +5,27 @@ if (typeof jQuery == 'undefined'){
 };
 
 
- $(() => {
-	// put jQuery in here
-});
-
 const $body = $('body'); //grabs the body element.
 $body.append("<div id='container'></div>") //appends a container element.
-const $container = $('container'); //grabs the container element.
+
+const $container = $('container'); //grabs the container element. CREATES?
 console.log($container); //checks for it.
 
-const $h1 = $('h1');
-$body.append('<h1>Hogwarts</h1>'); //you don't need a dollar sign on the right.
-console.log($h1);
+$("#container").append('<h1>Hogwarts</h1>'); //you don't need a dollar sign on the right.
+$("#container").append('<h2>Chris</h2>'); //appended Chris to the body.
+$("#container").append('<h3>Gryffindor</h3>'); //appends Gryffindor to body.
+$("#container").append('<h4 class="cat">Zombie</h4>');
+$("#container").append('<h4>FawksWand</h4>');
 
 
-const $h2 = $('h2');
-$body.append('<h2>Chris</h2>');
-const $h3 = $('h3');
-$body.append('<h3>Gryffindor</h3>');
-
-const $h4 = $('h4');
-$body.append('<h4>Zombie</h4>');
-//$h4.append('<class="cat">');
-//$('h4.Zombie').addClass('pet');
-$body.append('<h4>FawksWand</h4>');
-
-//neither adding classes work.
 
 
-console.log('hi');
+//Created a list and appened it to the container div. 
+$("#container").append('<ul id="list">STORAGE: TRUNK</ul>');
+
+$("#list").append('<li>butter beer</li');
+$("#list").append('<li class="secret">invisibility cloak</li');
+$("#list").append('<li class="secret">magic map</li');
+$("#list").append('<li class="secret">time turner</li');
+$("#list").append('<li class="cat">leash</li');
+$("#list").append("<li>Bertie Bott's Every Flavor [Jelly] Beans.</li");
