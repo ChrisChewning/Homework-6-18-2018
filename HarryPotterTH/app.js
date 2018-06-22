e1f0607d88f74c51c0c9e2d4fa06ddd18c3676ab
Chris <chewning.cl@gmail.com>
2018-06-21 21:15:05 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- HarryPotterTH/app.js
+++ HarryPotterTH/app.js
@@ -17,3 +17,20 @@ console.log($container); //checks for it.
 const $h1 = $('h1');
 $body.append('<h1>Hogwarts</h1>'); //you don't need a dollar sign on the right.
 console.log($h1);
+
+
+const $h2 = $('h2');
+$body.append('<h2>Chris</h2>');
+const $h3 = $('h3');
+$body.append('<h3>Gryffindor</h3>');
+
+const $h4 = $('h4');
+$body.append('<h4>Zombie</h4>');
+//$h4.append('<class="cat">');
+//$('h4.Zombie').addClass('pet');
+$body.append('<h4>FawksWand</h4>');
+
+//neither adding classes work.
+
+
+console.log('hi');
